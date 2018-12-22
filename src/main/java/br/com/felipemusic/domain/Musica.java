be134d48ffbc28d66a9cf0ebe23628fd55dc0bf9
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2018-12-22 12:06:24 -0300

    Classes de entidades finalizadas

--- src/main/java/br/com/felipemusic/domain/Musica.java
+++ src/main/java/br/com/felipemusic/domain/Musica.java
@@ -3,8 +3,10 @@ package br.com.felipemusic.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "musicas")
@@ -15,6 +17,31 @@ public class Musica {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Getter
+    @Setter
+    @Size(min = 2, max = 50)
+    @Column(nullable = false, length = 50)
+    private String titulo;
+
+    @Getter
+    @Setter
+    @Size(min = 2, max = 50)
+    @Column(nullable = false, length = 50)
+    private String banda;
+
+    @Getter
+    @Setter
+    @Range(min = 0, max = 10)
+    @Column(nullable = false)
+    private int nota;
+
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "id_playlist_fk")
+    private Playlist playlist;
+
 
 
 }
